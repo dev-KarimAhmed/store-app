019fef339204ad0824d51503140af6dbde050431
Karim Ahmed <devKarimAhmed@gmail.com>
2024-11-30 17:43:32 +0200

    env created done

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,19 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_store_app_with_graphql/core/app/env_variables.dart';
 import 'package:flutter_store_app_with_graphql/firebase_options.dart';
 import 'package:flutter_store_app_with_graphql/store.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
- await Firebase.initializeApp(
+  await EnvVariables.instance.init(environment: EnvironmentEnum.dev);
+  await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
-  runApp(const StoreQl());
+  await SystemChrome.setPreferredOrientations(
+    [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown],
+  ).then(
+    (_) => runApp(const StoreQl()),
+  );
 }
