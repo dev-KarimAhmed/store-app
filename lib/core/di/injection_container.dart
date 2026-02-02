2760a843710b445957f1e965cd55dc05cb2878bb
Karim Ahmed <devkarimahmed@gmail.com>
2026-02-02 11:57:56 +0200

    dio factory done

--- lib/core/di/injection_container.dart
+++ lib/core/di/injection_container.dart
@@ -1,13 +1,17 @@
 import 'package:flutter_store_app_with_graphql/core/app/app_cubit/app_cubit.dart';
+import 'package:flutter_store_app_with_graphql/core/graphql/api_service.dart';
+import 'package:flutter_store_app_with_graphql/core/graphql/dio_factory.dart';
 import 'package:get_it/get_it.dart';
 
 final sl = GetIt.instance;
-
 
 void setupInjection() {
   _initInjection();
 }
 
 void _initInjection() {
-   sl.registerFactory(AppCubit.new);
+  final dio = DioFactory.getDio();
+  sl
+    ..registerFactory(AppCubit.new)
+    ..registerLazySingleton<ApiService>(() => ApiService(dio));
 }
